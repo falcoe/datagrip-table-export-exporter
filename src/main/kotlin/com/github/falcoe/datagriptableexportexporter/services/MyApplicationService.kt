9850112ef89b3abae085a8c1c9dedf481c28db74
GitHub Action <action@github.com>
2021-03-02 08:24:08 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/falcoe/datagriptableexportexporter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.falcoe.datagriptableexportexporter.services
+
+import com.github.falcoe.datagriptableexportexporter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/falcoe/datagriptableexportexporter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/falcoe/datagriptableexportexporter/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.falcoe.datagriptableexportexporter.services
 
+import com.github.falcoe.datagriptableexportexporter.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
